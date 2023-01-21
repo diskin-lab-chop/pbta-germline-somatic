265ca4d8b8c171f8adce482a7c627041fe09bdaa
jharenza <jharenza@gmail.com>
2023-01-21 15:07:34 -0500

    add comment, delete unnecessary code

--- analyses/sample-distribution/01-create-cancer-distribution-plot.R
+++ analyses/sample-distribution/01-create-cancer-distribution-plot.R
@@ -21,22 +21,19 @@ hist <- read_tsv(file.path(root_dir,
                            "collapse-tumor-histologies", 
                            "results",
                            "germline-primary-plus-tumor-histologies-plot-groups-clin-meta.tsv"))
-  
 
+# add n per group in label  
 hist_counts <- hist %>%
   count(plot_group) %>%
   inner_join(hist) %>%
   mutate(plot_group_n = paste0(plot_group, " (n = ", n, ")")) %>%
   mutate(plot_group_n = fct_rev(fct_infreq(plot_group_n))) %>%
   unique()
-as.data.frame(table(hist_counts$plot_group_n))
 
 plot_group_palette <- hist_counts$plot_group_hex
 names(plot_group_palette) <- hist_counts$plot_group_n
 
-"#8f8fbf"
-
-tiff(file.path(plot_dir, "histology-distribution.tiff"), height = 2000, width = 3000, res = 300)
+tiff(file.path(plot_dir, "histology-distribution.tiff"), height = 1500, width = 2500, res = 300)
 ggplot(hist_counts, aes(x = plot_group_n, fill = plot_group_n)) +
   geom_bar(color = "black", show.legend = FALSE) +
   scale_fill_manual(values = plot_group_palette) + 
