8f418d638098d385619a70e099e9ac92afed8598
rjcorb <corbettr@chop.edu>
2023-03-06 15:20:59 -0500

    update R script

--- analyses/collapse-tumor-histologies/01-collapse-tumor-histologies.R
+++ analyses/collapse-tumor-histologies/01-collapse-tumor-histologies.R
@@ -131,13 +131,12 @@ tumor_clin_meta <- v11_hist %>%
          extent_of_tumor_resection, CNS_region, molecular_subtype) %>%
   dplyr::rename(sample_id_tumor = sample_id)
   
-ancestry <- read_tsv(file.path(input_dir, "CBTN_PNOC.somalier-ancestry.tsv")) %>%
-  filter(grepl("BS", `#sample_id`)) %>%
-  rename(Kids_First_Biospecimen_ID_normal = `#sample_id`)
+ancestry <- read_tsv(file.path(input_dir, "pbta-germline-ancestry-results.tsv")) %>%
+  rename(Kids_First_Biospecimen_ID_normal = Kids_First_Biospecimen_ID)
 
 final_hist <- combined_map %>%
   left_join(tumor_clin_meta) %>%
-  left_join(ancestry[,c("Kids_First_Biospecimen_ID_normal", "predicted_ancestry")], by = "Kids_First_Biospecimen_ID_normal") %>%
+  left_join(ancestry[,c("Kids_First_Biospecimen_ID_normal", "predicted_ancestry", "PC1", "PC2", "PC3", "PC4", "PC5", "PC6", "PC7", "PC8")], by = "Kids_First_Biospecimen_ID_normal") %>%
   write_tsv(file.path(results_dir, "germline-primary-plus-tumor-histologies-plot-groups-clin-meta.tsv"))
 
 
