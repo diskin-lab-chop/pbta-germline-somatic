1936c0d4f9442f21e408ec40c891e4ef95a91b95
rjcorb <corbettr@chop.edu>
2023-06-09 11:56:55 -0400

    edit bash command

--- analyses/hgnc-liftover/run_hgnc_liftover.sh
+++ analyses/hgnc-liftover/run_hgnc_liftover.sh
@@ -11,7 +11,7 @@ complete=(input/hgnc_complete_set_2023-04-01.txt)
 out_cpg=(results/cpg_liftover.tsv)
 
 # Run liftover
-python $scripts_dir/update_fusion_gene_symbols.py -g $complete -f $cpgs -o $out_cpg -u "Gene"
+python3 $scripts_dir/update_fusion_gene_symbols.py -g $complete -f $cpgs -o $out_cpg -u "Gene"
 
 # extract CPG name column
 awk -F "\t" '{print $1}' $out_cpg  | sed '1d' > results/cpg.txt
