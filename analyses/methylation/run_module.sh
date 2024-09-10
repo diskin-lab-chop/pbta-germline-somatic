f14e81bddfa373518620420e88c1c8becdab5a15
Ryan Corbett <corbettr@chop.edu>
2024-09-10 12:28:35 -0400

    update run script

--- analyses/methylation/run_module.sh
+++ analyses/methylation/run_module.sh
@@ -5,3 +5,6 @@ set -o pipefail
 
 # Format methylation data
 Rscript --vanilla 01-prepare-methylation.R
+
+# Assess global differential methylation
+R -e "rmarkdown::render('02-global-methylation.Rmd')"
