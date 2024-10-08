95018fd85b2e9fffdaff1fee4caaae098d1d6dd3
Ryan Corbett <corbettr@chop.edu>
2024-10-08 13:14:13 -0400

    update x-axes

--- analyses/cpg-enrichment/run_module.sh
+++ analyses/cpg-enrichment/run_module.sh
@@ -4,7 +4,7 @@ set -e
 set -o pipefail
 
 # Run CPG gene list enrichment
-Rscript 01-cpg-list-enr.R
+Rscript --vanilla 01-cpg-list-enr.R
 
 # Run gene- and pathway-level enrichment
-Rscript 02-gene-pathway-enrichment.R
+Rscript --vanilla 02-gene-pathway-enrichment.R
