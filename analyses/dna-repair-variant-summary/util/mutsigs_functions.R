4099847b78ad4d52aa7cca31a9a09a7099bc946e
Ryan Corbett <corbettr@chop.edu>
2024-12-31 14:40:45 -0500

    update plotting function, rm repair gene source-specific results

--- analyses/dna-repair-variant-summary/util/mutsigs_functions.R
+++ analyses/dna-repair-variant-summary/util/mutsigs_functions.R
@@ -24,29 +24,37 @@ data_summary <- function(x) {
 #' @export
 #'
 #' @examples
-plot_exposure_violin <- function(df, x, y, sig){
+plot_exposure_violin <- function(df, x, y, sig, group){
   
   comparisons <- ifelse(sig == "SBS3",
                             list(c("HR", "Non-HR")),
-                            list(c("MMR", "Non-MMR")))
-  
-  mmr_labels <- c(paste0("HR P/LP\n (n=", sum(df$germline_variant == "HR"), ")"), 
+                        ifelse(sig == "SBS30",
+                               list(c("BER", "Non-BER")),
+                                    list(c("MMR", "Non-MMR"))))
+
+  brca_labels <- c(paste0("HR P/LP\n (n=", sum(df$germline_variant == "HR"), ")"), 
                   paste0("HR WT\n (n=", sum(df$germline_variant == "Non-HR"), ")"))
   
-  brca_labels <- c(paste0("MMR P/LP\n (n=", sum(df$germline_variant == "MMR"), ")"), 
+  mmr_labels <- c(paste0("MMR P/LP\n (n=", sum(df$germline_variant == "MMR"), ")"), 
                    paste0("MMR WT\n (n=", sum(df$germline_variant == "Non-MMR"), ")"))
+  
+  ber_labels <- c(paste0("BER P/LP\n (n=", sum(df$germline_variant == "BER"), ")"), 
+                   paste0("BER WT\n (n=", sum(df$germline_variant == "Non-BER"), ")"))
   
   x_labels <- if (sig == "SBS3"){
     
-                   mmr_labels
-    
-  } else {
-    
-    brca_labels
-    
-  }
                    brca_labels
-  
+    
+    } else if (sig == "SBS30"){
+    
+    ber_labels
+      
+    } else {
+      
+      mmr_labels
+      
+    }
+
   vplot <- df %>%
     # generate violin plot and save 
     ggplot(aes(x = !!rlang::ensym(x), y = !!rlang::ensym(y), fill = !!rlang::ensym(x))) +
@@ -54,16 +62,19 @@ plot_exposure_violin <- function(df, x, y, sig){
                 show.legend = FALSE) +
     geom_boxplot(width=0.1, show.legend = FALSE) + 
     labs(x = 'Germline Variant Class', y = paste0(sig, " Exposure"),
-         title = "HGG, H3-WT") +
-    theme_minimal() +
-    theme(legend.position = 'none',
-          text = element_text(size = 12)) +
+         title = group) +
+  #  theme_minimal() +
+    # theme(legend.position = 'none',
+    #       text = element_text(size = 12),
+    #       plot.title = element_text(size = 8, hjust = 0.5)) +
     scale_x_discrete(labels = x_labels) +
     scale_fill_npg() +
     stat_summary(fun.data=data_summary,
                  show.legend = F) +
     scale_y_continuous(expand = expansion(mult = .2)) +
-    theme_Publication()
+    theme_Publication() +
+    theme(plot.title = element_text(size = 15, hjust = 0.5),
+          axis.title.x = element_text(size = 13)) 
 
     vplot <- vplot + stat_compare_means(method = "wilcox",
                                         comparisons = comparisons, size = 3,
