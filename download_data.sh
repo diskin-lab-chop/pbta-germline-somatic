cae9c0983e721d2e9f0e7807196cac2a791f215c
Ryan Corbett <rcorbett@childrensnational.org>
2025-06-23 15:28:40 -0400

    update to v10 release

--- download_data.sh
+++ download_data.sh
@@ -4,8 +4,8 @@ set -o pipefail
 
 # Use Children's bucket.
 URL=${PBTA_URL:-https://bti-openaccess-us-east-1-prd-rokita-lab.s3.us-east-1.amazonaws.com/pbta-germline-somatic}
-RELEASE=${PBTA_RELEASE:-v9}
-PREVIOUS=${PBTA_RELEASE:-v8}
+RELEASE=${PBTA_RELEASE:-v10}
+PREVIOUS=${PBTA_RELEASE:-v9}
 
 # Remove old symlinks in data
 find data -type l -delete
