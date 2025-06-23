3133eaa45a1bd089a5ddebf693cdf2a90ce43f3f
rjcorb <rcorbett@childrensnational.org>
2025-06-23 17:38:02 -0400

    update alt splicing module w/ DROSHA

--- analyses/alternative-splicing/run_module.sh
+++ analyses/alternative-splicing/run_module.sh
@@ -3,6 +3,14 @@
 set -e
 set -o pipefail
 
+# if processed rMATs does not exist, run 00-prepare_rmats.sh
+if [ -f "results/splice-events-rmats-cpgs.tsv.gz" ]; then
+    echo "Found processed rmats file. Proceeding..."
+else
+    echo "Processed rmats file does not exist. Running 00-filter-rmats.R..."
+    Rscript --vanilla 00-filter-rmats.R
+fi
+
 # Run AS event z-score calculation script
 Rscript -e "rmarkdown::render('01-calculate-PSI-zscores.Rmd')"
 
